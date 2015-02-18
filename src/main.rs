bae8b084367f31e9597eacc90a1410db1c8298a8
J/A <archer884@gmail.com>
2015-02-18 15:41:37 -0600

    Update to Chrono 0.2; working! /cheer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+#![feature(env)]
+
 extern crate chrono;
 
-use chrono::{UTC, Local, DateTime};
+use chrono::{ Date, Local, TimeZone, };
 
 fn main() {
-    let date = Local.ymd(2015, 2, 20);
+    let date = match parse_date(std::env::args().nth(1)) {
+        Ok(date) => date,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
 
-    println!("{}", date - Local::now());
+    println!("Days remaining: {}", (date - Local::today()).num_days());
+}
+
+fn parse_date(input: Option<String>) -> Result<Date<Local>, String> {
+    match input {
+        Some(input) => {
+            let values = input.split('/').filter_map(|i| i.parse().ok()).collect::<Vec<i32>>();
+
+            if values.len() != 3 {
+                return Err("Date must be of form yyyy/MM/dd".to_string());
+            }
+
+            Ok(Local.ymd(values[0], values[1] as u32, values[2] as u32))
+        },
+        _ => { Err("usage: ./date_diff yyyy/MM/dd".to_string()) },
+    }
 }
